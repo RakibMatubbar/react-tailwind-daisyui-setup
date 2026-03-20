d380b916954c8743251e4754f53377c38adf6719
RakibMatubbar <rakibmatubbar996@gmail.com>
2026-03-20 20:43:02 +0600

    Project Setup | Added Header Info

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className='bg-[#1D232A]'>
+        <div className='p-10 space-y-4'>
+          <p className='text-blue-400 text-xl font-bold'>SETUP GUIDE -REACT PROJECT</p>
+
+          <p className='text-white text-4xl font-bold'>Vite + React + <br />
+            <span className='text-blue-600'>Tailwind CSS</span> + DaisyUI</p>
+
+          <p className='text-gray-500'>A precise, step-by-step walkthrough for wiring Tailwind CSS v4 and
+            DaisyUl into a freshly scaffolded Vite React project - including the
+            peer-dependency fix that most tutorials skip.</p>
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
